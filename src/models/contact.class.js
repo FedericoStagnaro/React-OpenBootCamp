a750e961ad8c7fce145179cf548496ce3154bac6
Federico Leonardo Andres Stagnaro <62780657+FedericoStagnaro@users.noreply.github.com>
2022-11-14 22:31:35 +0000

    add contact class js

---
+++ src/models/contact.class.js
@@ -0,0 +1,15 @@
+class Contact {
+    name = ''
+    surname = ''
+    email = ''
+    online = false
+
+    constructor(name,surname,email,online) {
+        this.name = name
+        this.surname = surname
+        this.email = email
+        this.online = online
+    }
+}
+
+export default Contact
