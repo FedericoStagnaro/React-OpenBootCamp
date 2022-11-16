764ad86456bf310dfe4592a988f9a7d32d527a29
Federico Leonardo Andres Stagnaro <62780657+FedericoStagnaro@users.noreply.github.com>
2022-11-16 19:18:09 +0000

    add component and styles in line

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+/* eslint-disable no-unused-vars */
+
 import logo from './logo.svg';
 import './App.css';
 // import Greeting from './components/pure/greeting'
 // import GreetingF from './components/pure/greetingFunction'
-// import TaskList from './components/containers/task.list';
+
+
+import {TaskListClass, TaskListFunction} from './components/containers/task.list';
+import { GreetingStyled } from './components/pure/greeting.function.styled';
 // import Ejemplo1 from './hooks/ejemplo1.useState';
 // import Ejemplo2 from './hooks/ejemplo2.useEffect';
 // import Ejemplo3 from './hooks/ejemplo3.useContext';
 // import Ejemplo4 from './hooks/ejemplo4.propsChildren';
+// import LifeCycleExample from './components/pure/lifeCycleExample';
 
 function App() {
   return (
@@ -25,6 +31,11 @@ function App() {
           <li>Hijo 2</li>
           <li>Hijo 3</li>
         </Ejemplo4> */}
+        {/* <LifeCycleExample></LifeCycleExample> */}
+        {/* <TaskListClass></TaskListClass> */}
+        {/* <TaskListFunction></TaskListFunction> */}
+
+        <GreetingStyled name={'Federico'}></GreetingStyled>
 
       </header> 
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// añadimos bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+// estilos propios deben ir debajo de bootstrap
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
