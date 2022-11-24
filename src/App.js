ad2f27d779c65ec7e81078b524eea6ff84f3db43
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-11-24 11:16:12 -0300

    add bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import ContactList from './components/containers/contact.list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
         <ContactList></ContactList>
 
