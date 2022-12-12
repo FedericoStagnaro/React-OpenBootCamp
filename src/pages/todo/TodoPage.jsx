b7512e0b64703f5b70d75871bd3bdfc37f2f63b1
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-12 09:36:27 -0300

    add hook to dispatch actions

--- src/pages/todo/TodoPage.jsx
+++ src/pages/todo/TodoPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import TodoForm from '../../components/pure/form/TodoForm'
+import TodoList from '../../components/containers/TodoList'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo } from '../../store/actions/actions'
+
+
 
 export default function TodoPage() {
+    const dispatch = useDispatch()
+    const todos = useSelector(state => state.todoState)
 
     return (
         <div>
             <h1>TodoPage</h1>
             <div>
-                <TodoForm></TodoForm>
+                <TodoList todos={todos}></TodoList>
+            </div>
+            <div>
+                <h6>Form</h6>
+                <TodoForm submit={(text)=> dispatch(addTodo(text))}></TodoForm>
             </div>
         </div>
     )
