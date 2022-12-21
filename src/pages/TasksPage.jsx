a33d581b4837439e2ec7aee1749477fd9779ed7b
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-21 11:13:46 -0300

    task page with wrap

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { TaskListClass } from '../components/containers/task.list'
-import WrapperTaskList from '../components/containers/WrapperTaskList'
+import {WrapperTaskList} from '../components/containers/WrapperTaskList'
 
 export default function TasksPage() {
-
-
     return (
-        <div>
-            <h1>TasksPage</h1>
-            <WrapperTaskList></WrapperTaskList>
-            {/* <TaskListClass></TaskListClass> */}
-        </div>
+            <div>
+                <h1>TasksPage</h1>
+                <WrapperTaskList></WrapperTaskList>
+                {/* <TaskListClass></TaskListClass> */}
+            </div>  
     )
 }
