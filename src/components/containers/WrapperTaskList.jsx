fd783185b7e224737c97c9699fd62992a5e7b91f
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-21 11:13:57 -0300

    wrapper component with context

--- src/components/containers/WrapperTaskList.jsx
+++ src/components/containers/WrapperTaskList.jsx
@@ -1,10 +1,18 @@
 import React, { useReducer } from 'react'
 import LEVELS from '../../models/levels.enum'
 import { TaskListClass } from './task.list'
+import { TaskContext } from './TaskContext'
+import Task from "../../models/task.class";
+
+const defaultTask1 = new Task('Learn React', 'Learn about react components and hooks', LEVELS.URGENT, true)
+const defaultTask2 = new Task('Learn React2', 'Learn about react components and hooks2', LEVELS.NORMAL, false)
+const defaultTask3 = new Task('Learn React3', 'Learn about react components and hooks3', LEVELS.BLOCKING, false)
+const array = [defaultTask1, defaultTask2, defaultTask3]
 
 // initialState
 const initialState = {
-    tasks: [],
+    tasks: array,
+    loading: false,
     taskForm: {
         name: '',
         description: '',
@@ -14,20 +22,21 @@ const initialState = {
 
 // actions
 export const DELETE_TASK = 'DELETE_TASK'
-export const ADD_TASK = 'ADD_TASK'
+export const ADDING_TASK = 'ADDING_TASK'
+export const LOADING_TASK = 'LOADING_TASK'
+export const ADD_TASK = 'ADDED_TASK'
 export const FIELD_TASK = 'FIELD_TASK'
 
 // reducer
 const taskReducer = (state, action) => {
-    switch (state.type) { 
-        case ADD_TASK:
+    switch (action.type) { 
+
+        case ADD_TASK: 
+            const tempTasks = [...state.tasks].concat(action.newTask)
             return {
                 ...state,
-                taskForm: {
-                    name: action.taskForm.name,
-                    description: action.taskForm.description,
-                    level: action.taskForm.level
-                }
+                tasks: tempTasks,
+                loading: false
             }
         case DELETE_TASK:
             const index = state.tasks.indexOf(action.taskToDelete)
@@ -35,7 +44,7 @@ const taskReducer = (state, action) => {
             tempTask.splice(index, 1)
             return {
                 ...state,
-                tasks: [tempTask]
+                tasks: tempTask
             }
         case FIELD_TASK:
             return {
@@ -50,10 +59,14 @@ const taskReducer = (state, action) => {
     }
 }
 
-export default function WrapperTaskList(props) {
+
+
+export function WrapperTaskList(props) {
     const [state, dispatch] = useReducer(taskReducer, initialState)
 
     return (
-        <TaskListClass {...props} {...{ state, dispatch }}></TaskListClass>
+        <TaskContext.Provider value={{state:state, dispatch: dispatch}} >
+            <TaskListClass {...props} ></TaskListClass>
+        </TaskContext.Provider>
     )
 }
