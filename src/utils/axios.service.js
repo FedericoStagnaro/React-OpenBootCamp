f400507fc15747358a83da7e237666c7c3f62656
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-21 11:21:38 -0300

    clean table xD

--- src/utils/axios.service.js
+++
@@ -1,9 +0,0 @@
-import axiosConfig from "./axios.config";
-
-export const getRandomUser = () => {
-    return axiosConfig.get('/', {
-        validateStatus: (status)=> {
-            return status < 500
-        }
-    })
-}
