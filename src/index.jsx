a01c1745c8e39bc806e73c6dfa8c16760e04bded
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-22 10:23:21 -0300

    add folder structure

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React, { ReactDOM } from 'react';
+
+import App from './components/App';
+
+// TODO: importar estilos css
+import './styles/css/index.css';
+
+// TODO: Si trabajamos con redux, creamos el store y aplicamos el middleware de redux saga
+
+ReactDOM.render(<App />, document.getElementById('root'));
