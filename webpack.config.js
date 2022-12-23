311f5328319daefe596d0bc97bfa4c4853d6a45d
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-23 13:00:41 -0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,66 +5,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Para el template del html que va a utilizar el webpack
 const { SourceMapDevToolPlugin } = require('webpack'); //  para conocer el Source map de nuestro proyecto (desde el navegador)
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');// para reducir los css
+const ESLintPlugin = require('eslint-webpack-plugin');
+const eslintOptions = require('./.eslintrc.json');
 
 // configuracion del puerto
 const port = process.env.PORT || 3000;
 
-// exportar configuracion de webpack
+// Reglas para archivos jsx y js
+// tienen q pasar el linting para poder pasar
+const sourceMapLoader = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    enforce: 'pre',
+    use: [
+        {
+            loader: 'source-map-loader', // te permite vsualizar el codigo desde el navegador
+        },
+    ],
+};
 
+// reglas para archivos de JS y JSX
+// Reglas de babel ES y JSX
+const babelConfig = {
+    test: /(\.js|\.jsx)$/,
+    exclude: /node_modules/,
+    use: [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/env', '@babel/react'],
+            },
+        },
+    ],
+};
+
+// Reglas para los archivos CSS y SASS y SCSS para minificarlos y cargarlos en el bundle
+// Reglas para los archivos  de imagenes
+const cssConfig = {
+    test: /(\.css|\.scss)$/,
+    use: [
+        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+    ],
+};
+
+const fileConfig = {
+    test: /\.(png|jpe?g|gif)$/,
+    use: [
+        {
+            loader: 'file-loader',
+        },
+    ],
+};
+
+// exportar configuracion de webpack
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'bundle.[hash].js',
-        publicPath: '/',
+        filename: 'bundle.[contenthash].js',
+        // publicPath: '/',
     },
     context: path.resolve(__dirname),
     devServer: {
         port,
-        inline: true,
-        historyApiFallback: true,
     },
     devtool: 'eval-source-map',
     module: {
         rules: [
-            // Reglas para archivos jsx y js
-            // tienen q pasar el linting para poder pasar
-            {
-                test: /(\.js|\.jsx)$/,
-                exclude: /node_modules/,
-                enforce: 'pre',
-                use: [
-                    'eslint-loader',
-                    'source-map-loader', // te permite vsualizar el codigo desde el navegador
-                ],
-            },
-            // reglas para archivos de JS y JSX
-            // Reglas de babel ES y JSX
-            {
-                test: /(\.js|\.jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: [
-                        '@babel/env', '@babel/react',
-                    ],
-                },
-            },
-            // Reglas para los archivos CSS y SASS y SCSS para minificarlos y cargarlos en el bundle
-            {
-                test: /(\.css|\.scss)$/,
-                loader: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            // Reglas para los archivos  de imagenes
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                loader: ['file-loader'],
-
-            },
+            babelConfig,
+            sourceMapLoader,
+            cssConfig,
+            fileConfig,
         ],
     },
     plugins: [
@@ -74,9 +85,10 @@ module.exports = {
                 template: './index.html',
             },
         ),
+        // new ESLintPlugin(eslintOptions),
         new MiniCssExtractPlugin(
             {
-                filename: './css/styles.css',
+                filename: './src/css/styles.css',
             },
         ),
         new SourceMapDevToolPlugin(
