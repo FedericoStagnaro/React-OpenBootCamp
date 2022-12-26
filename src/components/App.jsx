80891e3f633d3088d928c2713fa462e959259f3f
FedericoStagnaro <federico.stagnaro.ar@gmail.com>
2022-12-26 17:19:45 -0300

    deploy de cambios app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 export default function App() {
     return (
       <div>
-        <h1>Proyecto Final</h1>
+        <h1>Proyecto Final React JS</h1>
       </div>
     );
 }
